34ced4f6de33fd8b0fa5862573ef4b3a5500d032
Tom <tom@realfactors.com>
2019-06-27 11:32:45 -0400

    fix x/y intercept bug not in metrics array

--- superset/viz.py
+++ superset/viz.py
@@ -943,20 +943,29 @@ class BubbleViz(NVD3Viz):
 
     def query_obj(self):
         form_data = self.form_data
-        d = super().query_obj()
-        d["groupby"] = [form_data.get("entity")]
-        if form_data.get("series"):
-            d["groupby"].append(form_data.get("series"))
-        self.x_metric = form_data.get("x")
-        self.y_metric = form_data.get("y")
-        self.z_metric = form_data.get("size")
-        self.entity = form_data.get("entity")
-        self.series = form_data.get("series") or self.entity
-        d["row_limit"] = form_data.get("limit")
-
-        d["metrics"] = list(set([self.z_metric, self.x_metric, self.y_metric]))
-        if not all(d["metrics"] + [self.entity]):
-            raise Exception(_("Pick a metric for x, y and size"))
+        d = super(BubbleViz, self).query_obj()
+        d['groupby'] = [
+            form_data.get('entity'),
+        ]
+        if form_data.get('series'):
+            d['groupby'].append(form_data.get('series'))
+        self.x_metric = form_data.get('x')
+        self.y_metric = form_data.get('y')
+        self.z_metric = form_data.get('size')
+        self.entity = form_data.get('entity')
+        self.series = form_data.get('series') or self.entity
+        self.x_intercept = form_data.get('x_intercept')
+        self.y_intercept = form_data.get('y_intercept')
+        d['row_limit'] = form_data.get('limit')
+        d['metrics'] = [
+            self.z_metric,
+            self.x_metric,
+            self.y_metric,
+            self.x_intercept,
+            self.y_intercept,
+        ]
+        if not all(d['metrics'] + [self.entity]):
+            raise Exception(_('Pick a metric for x, y and size'))
         return d
 
     def get_data(self, df):
